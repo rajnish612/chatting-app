0710426704a8481c8aa91b65afb50de3d3efe319
Rajnish Nath <rnish612@gmail.com>
2025-07-21 21:48:06 +0530

    _

--- my-react-app/src/components/User.jsx
+++ my-react-app/src/components/User.jsx
@@ -30,33 +30,59 @@ const User = ({}) => {
     <div className="flex justify-center w-screen h-screen p-2  items-center">
       <div className="w-[90%] sm:w-[80%]  md:w-[600px]  lg:w-[600px] xl:w-[700px] py-10 rounded-2xl flex flex-col justify-start  items-center shadow-2xl ring-7 ring-blue-300/35 ring-offset-2  ">
         {loading ? (
-          <div className="h-screen w-screen absolute inset-0 bg-white/10 backdrop-blur-md flex justify-center items-center">
-            <div className="p-4 rounded-full border-b-3 animate-spin border-blue-400 text-black h-10 w-10"></div>
-          </div>
+          <>
+            <div className="h-screen w-screen absolute inset-0 bg-white/10 backdrop-blur-md flex justify-center items-center">
+              <div className="p-4 rounded-full border-b-3 animate-spin border-blue-400 text-black h-10 w-10"></div>
+            </div>
+          </>
         ) : error ? (
           <>
-            <div className="bg-red-500 animate-pulse rounded-full p-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="white"
-                class="size-20"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
-                />
-              </svg>
-            </div>
+            <div className="h-screen w-screen absolute inset-0 flex-col bg-white/10 backdrop-blur-md flex justify-center items-center">
+              <div className="bg-red-500 animate-pulse rounded-full p-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="white"
+                  class="size-20"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
+                  />
+                </svg>
+              </div>
 
-            <span className="text-slate-500 mt-5 text-xl md:text-3xl font-black">
-              USER NOT FOUND !!{" "}
-            </span>
+              <span className="text-slate-500 mt-5 text-xl md:text-3xl font-black">
+                USER NOT FOUND !!{" "}
+              </span>
+            </div>
           </>
         ) : (
+          //   <>
+          //     <div className="bg-red-500 animate-pulse rounded-full p-2">
+          //       <svg
+          //         xmlns="http://www.w3.org/2000/svg"
+          //         fill="none"
+          //         viewBox="0 0 24 24"
+          //         stroke-width="1.5"
+          //         stroke="white"
+          //         class="size-20"
+          //       >
+          //         <path
+          //           stroke-linecap="round"
+          //           stroke-linejoin="round"
+          //           d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
+          //         />
+          //       </svg>
+          //     </div>
+
+          //     <span className="text-slate-500 mt-5 text-xl md:text-3xl font-black">
+          //       USER NOT FOUND !!{" "}
+          //     </span>
+          //   </>
           <>
             <div className="rounded-full bg-slate-100 ring-1 ring-amber-300 ring-offset-3  h-30 w-30 xl:h-35 md:w-35">
               <img
