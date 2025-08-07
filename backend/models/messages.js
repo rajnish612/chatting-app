14ed061ced572d509c80e533088e99cfd5917706
Rajnish Nath <rnish612@gmail.com>
2025-08-07 22:09:09 +0530

    modified messages schema

--- backend/models/messages.js
+++ backend/models/messages.js
@@ -7,6 +7,8 @@ const messageSchema = new mongoose.Schema({
   isSeen: { type: Boolean, default: false },
   deletedFor: { type: [String], default: [] },
   deletedForEveryone: { type: Boolean, default: false },
+  messageType: { type: String, default: null },
+  audioId: { type: String },
 });
 const Message = mongoose.model("Message", messageSchema);
 export default Message;
