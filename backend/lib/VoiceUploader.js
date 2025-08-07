164add3dcf91c75c35b1fb3d543fb39566eb6f40
Rajnish Nath <rnish612@gmail.com>
2025-08-07 21:54:27 +0530

    created voice message uploader helper

---
+++ backend/lib/VoiceUploader.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+export const upload = async (audioBuffer) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      {
+        resource_type: "video",
+
+        upload_preset: "chatting_app_preset",
+        folder: "chattingApp",
+      },
+      (error, result) => {
+        if (error) return reject(error);
+        console.log(result.public_id, result.secure_url);
+
+        return resolve({ public_id: result.public_id, url: result.secure_url });
+      }
+    );
+    stream.end(audioBuffer);
+  });
+};
