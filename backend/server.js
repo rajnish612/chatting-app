eff387e93d9cf0ac074a37cb7aa630641714cb6e
Rajnish Nath <rnish612@gmail.com>
2025-08-11 10:24:12 +0530

    fixed some session bugs

--- backend/server.js
+++ backend/server.js
@@ -76,7 +76,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: false,
+      secure: true,
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
     },
