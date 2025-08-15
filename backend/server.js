edfa685f135f1e73363556275fe3c856b9ce3d08
Rajnish Nath <rnish612@gmail.com>
2025-08-15 09:40:40 +0530

    _

--- backend/server.js
+++ backend/server.js
@@ -72,13 +72,13 @@ app.use(
 );
 app.use(
   session({
-    secret: "secret",
+    secret: process.env.SESSION_SECRET || "secret",
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: false,
+      secure: process.env.NODE_ENV === 'production',
       httpOnly: true,
-      // sameSite: 'none',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
     },
   })
